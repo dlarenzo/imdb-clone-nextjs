87350f0b736569940ef3541d91f32f7440c2957e
LaRenzo <dlarenzo@outlook.com>
2025-01-18 22:16:33 -0700

    add mongodb

--- src/lib/actions/user.js
+++ src/lib/actions/user.js
@@ -6,7 +6,6 @@ import { connect } from "../mongodb/mongoose";
 
 // EXPORT CREATE USER HANDLER
 export const createOrUpdateUser = async (
-  // CLERK INFORMATION
   id,
   first_name,
   last_name,
@@ -14,15 +13,11 @@ export const createOrUpdateUser = async (
   email_addresses
 ) => {
   try {
-    // CONNECT TO MONGODB
     await connect();
-
-    // CREATE OR UPDATE USER
     const user = await User.findOneAndUpdate(
       { clerkId: id },
       {
         $set: {
-          clerkId: id,
           firstName: first_name,
           lastName: last_name,
           profilePicture: image_url,
@@ -33,19 +28,14 @@ export const createOrUpdateUser = async (
     );
     return user;
   } catch (error) {
-    console.error("Error: Could not create or update user:", error);
+    console.log("Error: Could not create or update user:", error);
   }
 };
-
-// EXPORT DELETE USER HANDLER
 export const deleteUser = async (id) => {
   try {
-    // CONNECT TO MONGODB
     await connect();
-
-    // DELETE USER
-    await User.deleteOneAndDelete({ clerkId: id });
+    await User.findOneAndDelete({ clerkId: id });
   } catch (error) {
-    console.error("Error: Could not delete user:", error);
+    console.log("Error: Could not delete user:", error);
   }
 };
