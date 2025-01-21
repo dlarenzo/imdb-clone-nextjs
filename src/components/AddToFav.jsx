407dece7462db07a26e56621f957cfd122c62f1b
LaRenzo <dlarenzo@outlook.com>
2025-01-21 14:12:06 -0700

    working on favorites route 2

--- src/components/AddToFav.jsx
+++ src/components/AddToFav.jsx
@@ -1,31 +1,21 @@
-// CLIENT SIDE COMPONENT SO NO NEED FOR ASYNC
 "use client";
-import React from "react";
-
-// IMPORT STATE AND EFFECT
 import { useState, useEffect } from "react";
-// useUser FROM CLERK
 import { useUser } from "@clerk/nextjs";
-// IMPORT USE ROUTER
 import { useRouter } from "next/navigation";
 
-// THE PARAMS BEING PASSED THROUGH ARE FROM THE [ID OF MOVIE] PAGE
 export default function AddToFav({
   movieId,
   title,
+  image,
   overview,
   releaseDate,
   voteCount,
 }) {
-  // IS THIS MOVIE A FAVORITE OF THE USER OR NOT
   const [isFav, setIsFav] = useState(false);
-  // LOADING STATE EFFECT
-  const [loading, setLoading] = useState(true);
-  // AUTHENTICATION STATUS FROM CLERK
+  const [isLoading, setIsLoading] = useState(false);
   const { isSignedIn, user, isLoaded } = useUser();
   const router = useRouter();
 
-  // USE EFFECT IS LOGGED IN OR NOT
   useEffect(() => {
     setIsLoading(true);
     if (isLoaded && isSignedIn && user) {
@@ -36,25 +26,19 @@ export default function AddToFav({
     }
   }, [movieId, isLoaded, isSignedIn, user]);
 
-  // WHEN USER CLICKS ON THE BUTTON
   const handleFavClick = async () => {
-    // IF NOT SIGNED IN REDIRECT TO SIGN IN PAGE
     setIsLoading(true);
-
     if (!isSignedIn) {
-      setLoading(false);
+      setIsLoading(false);
       router.push("/sign-in");
       return;
     }
-
-    // IF AUTHENTICATED FETCH URL
     try {
       const res = await fetch("/api/user/fav", {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
-        // SEND THE INFORMATION BELOW AND SAVE IT TO THE DATABASE
         body: JSON.stringify({
           movieId,
           title,
@@ -65,14 +49,13 @@ export default function AddToFav({
         }),
       });
 
-      // IF IS FAVORITE IS ALREADY A FAVORITE REMOVE IT
       if (res.ok) {
         setIsFav(!isFav);
       } else {
-        console.error("Failed to update your favorites");
+        console.error("Failed to update favorites");
       }
     } catch (error) {
-      console.error("Error", error);
+      console.error("Error:", error);
     } finally {
       setIsLoading(false);
     }
@@ -82,12 +65,11 @@ export default function AddToFav({
     <div>
       <button
         onClick={handleFavClick}
-        className={`p-2 rounded ${
+        className={`p-2 rounded  ${
           isFav ? "bg-red-300 dark:bg-red-600" : "bg-gray-300 dark:bg-gray-600"
         }`}
         disabled={isLoading}
       >
-        {/* ADD REMOVE FROM FAVORITES */}
         {isLoading
           ? "Loading..."
           : isFav
